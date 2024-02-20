a2eeb324573bda6d08a4f37c5325f88c42d227a8
DCritt <dcritte@clemson.edu>
2024-02-20 14:20:49 -0500

    Changed Health Stamina and Oxygen meters
    Instead of using GameObject.FindGameObjectWIthTag() I used a serialized field private player variable to get a reference to the player. This causes less strain on the possessor as using the find game object function is costly.

--- Oleum/Assets/Scripts/UIScripts/HealthBarScript.cs
+++ Oleum/Assets/Scripts/UIScripts/HealthBarScript.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class HealthBarScript : MonoBehaviour
 {
 
-    private Player player;
+    [SerializeField] private Player player;
     private float currScale;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
 
     }
 
--- Oleum/Assets/Scripts/UIScripts/StaminaBarScript.cs
+++ Oleum/Assets/Scripts/UIScripts/StaminaBarScript.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class StaminaBarScript : MonoBehaviour
 {
 
-    private Player player;
+    [SerializeField] private Player player;
     private float currScale;
 
     // Start is called before the first frame update
     void Start()
     {
         
-        player = player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
 
     }
 
