5b47c72305c7d347a1e14e97a87a2c9ced411adc
DCritt <dcritte@clemson.edu>
2024-02-29 13:06:35 -0500

    Fixed Errors inclusions

--- Oleum/Assets/Scripts/Systems/MapGenerator.cs
+++ Oleum/Assets/Scripts/Systems/MapGenerator.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.Tilemaps;
-using static UnityEditor.FilePathAttribute;
 
 public class MapGenerator : MonoBehaviour
 {
